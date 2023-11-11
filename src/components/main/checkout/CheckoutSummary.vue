f62fe2529efc0dcc955c94b13db7b23b03b8c7fe
Michal Trubini <mchl.trubini@gmail.com>
2023-11-11 12:48:28 +0100

    checkout summary WIP

--- src/components/main/checkout/CheckoutSummary.vue
+++ src/components/main/checkout/CheckoutSummary.vue
@@ -1,9 +1,32 @@
-<template></template>
+<template>
+  <div class="pagePadding maxLimit">
+    <div class="p-6 bg-white rounded-lg">
+      <h2 class="text-lg tracking-[1.3px] mb-8">summary</h2>
+      <div></div>
+      <div >
+        <div class="flex justify-between items-center mb-2">
+            <p class="uppercase text-[15px]">total</p>
+            <p class="font-bold text-blackish">$ 5,396</p>
+        </div>
+        <div class="flex justify-between items-center mb-2">
+            <p class="uppercase text-[15px]">shipping</p>
+            <p class="font-bold text-blackish">$ 0</p>
+        </div>
+        <div class="flex justify-between items-center mb-6">
+            <p class="uppercase text-[15px]">var (included)</p>
+            <p class="font-bold text-blackish">$ 1,079</p>
+        </div>
+        <div class="flex justify-between items-center mb-8">
+            <p class="uppercase text-[15px]">grand total</p>
+            <p class="font-bold text-brick">$ 5,446</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
-
 export default {
   name: "Checkout",
 };
-
 </script>
--- src/views/CheckoutPage.vue
+++ src/views/CheckoutPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-whitish">
+  <div class="bg-whitish flex flex-col gap-8">
     <CheckoutForm />
     <CheckoutSummary />
   </div>
