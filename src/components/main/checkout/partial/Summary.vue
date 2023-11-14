c36370b2db81217ff124cf02486089cba3f5d49a
Michal Trubini <mchl.trubini@gmail.com>
2023-11-14 18:54:36 +0100

    checkout summary

--- src/components/main/checkout/partial/Summary.vue
+++ src/components/main/checkout/partial/Summary.vue
@@ -3,20 +3,20 @@
     <h2 class="text-lg tracking-[1.3px] mb-8">summary</h2>
     <div class="flex flex-col gap-6 mb-8">
       <div
-        v-for="(item, index) in cartData"
+        v-for="(item, index) in cartItems"
         :key="index"
         class="flex gap-4 items-center justify-start"
       >
         <img
-          :src="item.image"
+          :src="item.cartImage"
           alt="product image"
           class="w-[64px] rounded-lg"
         />
         <div class="flex justify-between items-start w-full">
           <div>
-            <p class="font-bold text-black">{{ item.name }}</p>
+            <p class="font-bold text-black">{{ item.productTitle }}</p>
             <p class="font-bold">
-              {{ "$" + " " + item.price.toLocaleString() }}
+              {{ "$" + " " + item.productPrice.toLocaleString() }}
             </p>
           </div>
           <p class="font-bold">{{ "x" + item.quantity }}</p>
@@ -26,19 +26,19 @@
     <div>
       <div class="flex justify-between items-center mb-2">
         <p class="uppercase text-[15px]">total</p>
-        <p class="font-bold text-blackish">$ 5,396</p>
+        <p class="font-bold text-blackish">{{`$ ${itemsTotalPrice.toLocaleString()}` }}</p>
       </div>
       <div class="flex justify-between items-center mb-2">
         <p class="uppercase text-[15px]">shipping</p>
-        <p class="font-bold text-blackish">$ 0</p>
+        <p class="font-bold text-blackish">{{`$ ${shipping.toLocaleString()}` }}</p>
       </div>
       <div class="flex justify-between items-center mb-6">
-        <p class="uppercase text-[15px]">var (included)</p>
-        <p class="font-bold text-blackish">$ 1,079</p>
+        <p class="uppercase text-[15px]">vat (included)</p>
+        <p class="font-bold text-blackish">{{`$ ${calculatedVAT.toLocaleString()}` }}</p>
       </div>
       <div class="flex justify-between items-center mb-8">
         <p class="uppercase text-[15px]">grand total</p>
-        <p class="font-bold text-brick">$ 5,446</p>
+        <p class="font-bold text-brick">{{`$ ${grandTotal.toLocaleString()}` }}</p>
       </div>
       <button
         class="bg-brick py-[15px] text-white uppercase text-center text-[13px] tracking-[1px] font-bold inline-block w-full hover:bg-lightBrick"
@@ -50,6 +50,7 @@
 </template>
 
 <script lang="ts">
+import { mapGetters } from "vuex";
 export default {
   name: "Checkout",
   data() {
@@ -74,7 +75,20 @@ export default {
           quantity: 1,
         },
       ],
+      shipping: 19.99,
     };
+  },
+  computed: {
+    ...mapGetters(["cartItems","getItemsTotalPrice"]),
+    itemsTotalPrice() {
+      return this.getItemsTotalPrice;
+    },
+    calculatedVAT() {
+      return (this.getItemsTotalPrice * 0.2);
+    },
+    grandTotal() {
+      return (this.itemsTotalPrice + this.shipping);
+    },
   },
 };
 </script>
