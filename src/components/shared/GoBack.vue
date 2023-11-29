6a9508d4cbd2a1452ebd655cb40193806ffc0562
Michal Trubini <mchl.trubini@gmail.com>
2023-11-29 14:16:14 +0100

    go back functionality

--- src/components/shared/GoBack.vue
+++ src/components/shared/GoBack.vue
@@ -1,13 +1,20 @@
 <template>
+<router-link to="#" @click.native="goBack">
   <p
     class="maxLimit py-4 md:py-8 dt:pt-[80px] dt:pb-[56px] hover:cursor-pointer hover:text-brick hover:font-bold"
   >
     Go Back
   </p>
+</router-link>
 </template>
 
 <script lang="ts">
 export default {
   name: "GoBack",
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
